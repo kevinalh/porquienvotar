2b3932d9013c761033225a1695d6146c7096320f
Kevin <kevinalh@hotmail.com>
2016-02-19 15:10:38 -0500

    minor

--- porquienvoto/settings/production.py
+++ porquienvoto/settings/production.py
@@ -15,7 +15,7 @@ def get_env_variable(var_name):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_env_variable("MY_KEY")
 
-ROOT_URLCONF = 'porquienvoto.urls'
+ROOT_URLCONF = "porquienvoto.urls"
 
 WSGI_APPLICATION = '/var/www/kevinalh_pythonanywhere_com_wsgi.py'
 DEBUG = False
