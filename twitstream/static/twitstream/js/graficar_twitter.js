f4d3a3e9dca2364486e1382cea459c0d9534fe37
Kevin <kevinalh@hotmail.com>
2016-03-10 18:25:40 -0500

    sliders

--- twitstream/static/twitstream/js/graficar_twitter.js
+++ twitstream/static/twitstream/js/graficar_twitter.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
-	google.charts.load('current', {'packages':['corechart'], 'language': 'es'});
+	google.charts.load('current', {'packages':['corechart', 'controls'], 'language': 'es'});
 	// Set a callback to run when the Google Visualization API is loaded.
-    google.charts.setOnLoadCallback(drawChart);
-	
-    function drawChart() {
+    // google.charts.setOnLoadCallback(drawChart);
+    google.charts.setOnLoadCallback(drawDashboard);
+    
+    function construirDataTable() {
     	var data = new google.visualization.DataTable();
     	
     	data.addColumn('datetime', 'Fecha y hora');
@@ -28,6 +29,62 @@ $(document).ready(function(){
     	}
     	
     	data.addRows(tabla);
+    	return data;
+    }
+    
+    function drawDashboard() {
+    	var data = construirDataTable();
+    	
+    	var dashboard = new google.visualization.Dashboard(document.getElementById('dashboard'));
+    	
+        var options = {
+			title: 'Popularidad en Twitter',
+			legend: { position: 'bottom' },
+			height: '100%',
+			width: '100%',
+			hAxis: {
+			    format: 'M/d/yy HH:mm',
+			      gridlines: {
+			            count: -1,
+			            units: {
+			              days: {format: ['MMM dd']},
+			              hours: {format: ['HH:mm', 'ha']},
+			            }
+			          },
+			          minorGridlines: {
+			            units: {
+			              hours: {format: ['hh:mm a', 'ha']},
+			              minutes: {format: ['HH:mm a Z', ':mm']}
+			            }
+			          },
+			},
+			vAxis: {
+				gridlines: {color: 'none'},
+			    minValue: 0
+			},
+        };
+    	
+        var dateSlider = new google.visualization.ControlWrapper({
+            'controlType': 'DateRangeFilter',
+            'containerId': 'controles',
+            'options': {
+            	'filterColumnLabel': 'Fecha y hora'
+            }
+          });
+        
+        var chart_wrapper = new google.visualization.ChartWrapper({
+            'chartType': 'LineChart',
+            'containerId': 'timeline_twitter',
+            'options': options,
+        });
+        
+        dashboard.bind(dateSlider, chart_wrapper);
+
+        dashboard.draw(data);
+    }
+    /*
+    function drawChart() {
+    	var data = construirDataTable();
 
         var options = {
 			title: 'Popularidad en Twitter',
@@ -56,9 +113,12 @@ $(document).ready(function(){
 			},
 			
         };
-
+        
+        var view = new google.visualization.DataView(data);
+        
         var chart = new google.visualization.LineChart(document.getElementById("timeline_twitter"));
-
-        chart.draw(data, options);
+                
+        chart.draw(view, options);
     }
+    */
 });    
