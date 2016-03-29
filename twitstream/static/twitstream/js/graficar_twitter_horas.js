096f4db4ed74b49d6de3999ccb2e3ce465cd65af
Kevin <kevinalh@hotmail.com>
2016-03-29 13:38:55 -0500

    Ultimo_dia

--- twitstream/static/twitstream/js/graficar_twitter_horas.js
+++ twitstream/static/twitstream/js/graficar_twitter_horas.js
@@ -10,16 +10,16 @@ $(document).ready(function(){
     function construirDataTable() {
     	var data = new google.visualization.DataTable();
     	
-    	data.addColumn('date', 'Fechas');
+    	data.addColumn('datetime', 'Fechas');
     	
     	
 		for (var i = 0; i < candidatos_length; i++) {
 			data.addColumn('number', candidatos[i]);
 		}
     	tabla = [];
-    	for (var j = 1; j < tiempos_length; j++) {
+    	for (var j = 0; j < tiempos_length; j++) {
     		hr = tiempos[j].split(',')
-    		tabla_tiempo = [new Date(Number(hr[0]),Number(hr[1])-1,Number(hr[2]))]
+    		tabla_tiempo = [new Date(Number(hr[0]),Number(hr[1])-1,Number(hr[2]),Number(hr[3]))]
     		for (var i = 0; i < candidatos_length; i++) {
     			tabla_tiempo.push(datatwitter[tiempos[j]][candidatos[i]]);
     		}
@@ -43,15 +43,22 @@ $(document).ready(function(){
     	
         var options = {
 			title: 'Popularidad en Twitter',
-			// legend: { position: 'bottom' },
+			legend: { position: 'bottom' },
 			height: '100%',
 			width: '100%',
 			hAxis: {
-			    format: 'd/M/yy',
+			    format: 'M/d/yy HH:mm',
 			      gridlines: {
 			            count: -1,
 			            units: {
 			              days: {format: ['MMM dd']},
+			              hours: {format: ['HH:mm', 'ha']},
+			            }
+			          },
+			          minorGridlines: {
+			            units: {
+			              hours: {format: ['hh:mm a', 'ha']},
+			              minutes: {format: ['HH:mm a Z', ':mm']}
 			            }
 			          },
 			},
@@ -76,7 +83,7 @@ $(document).ready(function(){
             'options': options,
         });
         
-        var dateFormatter = new google.visualization.DateFormat({pattern: 'dd/MM/yyyy'});
+        var dateFormatter = new google.visualization.DateFormat({pattern: 'dd/MM/yyyy HH:mm'});
     	dateFormatter.format(data, 0);
         
         dashboard.bind(dateSlider, chart_wrapper);
--- twitstream/views.py
+++ twitstream/views.py
@@ -6,20 +6,47 @@ from quiz.models import Candidato
 from .models import Tweet
 
 import json
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import pytz
 
-# import pdb
+import pdb
 
 
 def twitter_candidatos(request):
     lista_candidatos = get_list_or_404(Candidato, entra_candidato=True)
-    tiempo_inicio = make_aware(datetime(2016, 3, 8, 0, 0, 0), utc)
-    diferencia_tiempo = timedelta(hours=1)
-    tiempo_final = Tweet.objects.filter(analizado=True).aggregate(Max('created_at'))['created_at__max']
-    final_range = tiempo_final - tiempo_inicio
+    # Por horas:
+    # ===========================================================================
+    # tiempo_inicio = make_aware(datetime(2016, 3, 8, 0, 0, 0), utc)
+    # diferencia_tiempo = timedelta(hours=1)
+    # tiempo_final = Tweet.objects.filter(analizado=True).aggregate(Max('created_at'))['created_at__max']
+    # final_range = tiempo_final - tiempo_inicio
+    # pedazos = int(final_range / diferencia_tiempo)
+    # lista_tiempos = [tiempo_inicio + x*diferencia_tiempo for x in range(1, pedazos)]
+    # diccionario = {}
+    # tweets = Tweet.objects.filter(analizado=True)
+    # for tiempo in lista_tiempos:
+    #     en_rango = tweets.filter(created_at__range=(tiempo - diferencia_tiempo, tiempo))
+    #     dic_candidatos = {}
+    #     for candidato in lista_candidatos:
+    #         menciones = en_rango.filter(candidatos=candidato.id).count()
+    #         dic_candidatos[candidato.alias_candidato] = menciones
+    #     # pdb.set_trace()
+    #     localizado = tiempo.astimezone(pytz.timezone('America/Lima'))
+    #     diccionario[localizado.strftime("%Y,%m,%d,%H")] = dic_candidatos
+    # # pdb.set_trace()
+    # dic_json = json.dumps(diccionario, ensure_ascii=False)
+    # candidatos = json.dumps(list(dic_candidatos.keys()), ensure_ascii=False)
+    # tiempos = json.dumps([tiempo.astimezone(pytz.timezone('America/Lima')).strftime("%Y,%m,%d,%H")
+    #                       for tiempo in lista_tiempos])
+    # ===========================================================================
+    # Por dias:
+    dia_inicio = make_aware(datetime(2016, 3, 8), utc)
+    diferencia_tiempo = timedelta(days=1)
+    # dia_final = make_aware(date.today(), utc)
+    dia_final = Tweet.objects.filter(analizado=True).aggregate(Max('created_at'))['created_at__max']
+    final_range = dia_final - dia_inicio
     pedazos = int(final_range / diferencia_tiempo)
-    lista_tiempos = [tiempo_inicio + x*diferencia_tiempo for x in range(1, pedazos)]
+    lista_tiempos = [dia_inicio + x*diferencia_tiempo for x in range(1, pedazos)]
     diccionario = {}
     tweets = Tweet.objects.filter(analizado=True)
     for tiempo in lista_tiempos:
@@ -29,12 +56,12 @@ def twitter_candidatos(request):
             menciones = en_rango.filter(candidatos=candidato.id).count()
             dic_candidatos[candidato.alias_candidato] = menciones
         # pdb.set_trace()
-        localizado = tiempo.astimezone(pytz.timezone('America/Lima'))
-        diccionario[localizado.strftime("%Y,%m,%d,%H")] = dic_candidatos
-    # pdb.set_trace()
+        # localizado = tiempo.astimezone(pytz.timezone('America/Lima'))
+        # diccionario[localizado.strftime("%Y,%m,%d")] = dic_candidatos
+        diccionario[tiempo.strftime("%Y,%m,%d")] = dic_candidatos
     dic_json = json.dumps(diccionario, ensure_ascii=False)
     candidatos = json.dumps(list(dic_candidatos.keys()), ensure_ascii=False)
-    tiempos = json.dumps([tiempo.astimezone(pytz.timezone('America/Lima')).strftime("%Y,%m,%d,%H")
+    tiempos = json.dumps([tiempo.astimezone(pytz.timezone('America/Lima')).strftime("%Y,%m,%d")
                           for tiempo in lista_tiempos])
     colores = json.dumps({candidato.alias_candidato: candidato.partido_candidato.color_partido
                           for candidato in lista_candidatos})
